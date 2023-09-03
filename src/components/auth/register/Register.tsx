983a5ad63b858b1a4ebf41bf4b265b7db0ab2334
TopyrkinaAlina <toperkinaaa@gmail.com>
2023-09-03 23:49:36 +0400

    T-10724173: внесение изменений по PR

--- src/components/auth/register/Register.tsx
+++ src/components/auth/register/Register.tsx
@@ -1,27 +1,30 @@
-import React, { useEffect} from 'react';
+import React, { useEffect } from 'react';
 import './register.css';
-import { TextField, Button, Typography,Box } from '@mui/material';
+import { TextField, Button, Typography } from '@mui/material';
 import { z } from 'zod';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import zodNameValidation from './validation.message';
+import MessagesRegister from './register.messages';
 
+const fieldSchema = {
+  username: zodNameValidation('Имя'),
+  surname: zodNameValidation('Отчество'),
+  patronymic: zodNameValidation('Фамилия'),
+  email: z.string().email({ message: MessagesRegister.errors.email }),
+  password: z.string().min(12, { message: 'Пароль слишком короткий' }),
+  confirmPassword: z
+    .string()
+    .min(12, { message: 'Повторите пароль' }),
+};
 
-const formSchema = z.object({
-  username: z.string().min(1, { message: 'Обязательное поле' })
-  .max(100),
-  surname: z.string().min(1, { message: 'Обязательное поле' })
-  .max(100),
-  patronymic: z.string().min(1,{ message: 'Обязательное поле' })
-  .max(100),
-  email: z.string().email({ message:'Некорректный email'}),
-  password: z.string().min(12,{ message: 'Пароль слишком короткий'}),
-  confirmPassword: z.string().min(12, { message: 'Повторите пароль'}),
-}).refine((data) => data.password === data.confirmPassword, {
-  path: ['confirmPassword'],
-  message: 'Введенные пароли не совпадают',
-})
-
-type FormSchema = z.infer<typeof formSchema>
+// Применение эффектов к полям
+const formSchema = z
+  .object(fieldSchema)
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ['confirmPassword'],
+    message: 'Введенные пароли не совпадают',
+  });
 
 function Register() {
   const {
@@ -30,144 +33,186 @@ function Register() {
     reset,
     setFocus,
     formState: { isDirty, isSubmitting, errors },
-  } = useForm<FormSchema>({ resolver: zodResolver(formSchema) })
+    trigger,
+  } = useForm<ShemaType>({
+    resolver: zodResolver(formSchema),
+})
 
   // обработчик отправки формы
-  const onSubmit: SubmitHandler<FormSchema> = (data) => {
+  const onSubmit: SubmitHandler<ShemaType> = (data) => {
     console.log(data)
     reset()
   }
 
-
   useEffect(() => {
     // устанавливаем фокус на первое поле (имя пользователя) после монтирования компонента
-    setFocus("username")
-  }, [setFocus])
+    setFocus('username');
+  }, [setFocus]);
 
   return (
-    <div className='root'> 
-       <Box
-          display='flex'
-          justifyContent='center'
-          alignItems='center'
-          flexDirection='column'
-          maxWidth={540}
-          margin='auto'
-          padding={5}
-          borderRadius={5}
-          boxShadow='5px 5px 10px #ccc'
-        >
-        <Typography variant="h2" fontFamily='Popins' textAlign='center'>Регистрация</Typography>
-        <Typography variant="body1" marginBottom={3} fontFamily='Popins' textAlign='center'>Введите данные для регистрации</Typography>
-        <form className='form' onSubmit={handleSubmit(onSubmit)}>
-        <TextField 
-        /* eslint-disable react/jsx-props-no-spreading */
-        {...register('username')}
-        disabled={isSubmitting}
-        id="username"
-        fullWidth margin="normal"
-        label="Имя *"
-        variant="outlined"
-        placeholder="Введите ваше Имя"
-        aria-invalid={errors.username ? 'true' : 'false'}
-        />
-        {errors.username && (
-          <span role='alert' className='error'>
-          {errors.username?.message}
-          </span>
-        )}
-        <TextField
-        {...register('surname')}
-        disabled={isSubmitting}
-        id='surname'
-        fullWidth margin='normal'
-        label="Отчество *"
-        variant="outlined"
-        placeholder="Введите ваше Отчество"
-        aria-invalid={errors.surname ? 'true' : 'false'}
-        />
-        {errors.surname && (
-        <span role='alert' className='error'>
-          {errors.surname?.message}
-        </span>
-        )}
-        <TextField
-        {...register('patronymic')}
-        disabled={isSubmitting}
-        id='patronymic'
-        fullWidth margin='normal'
-        label="Фамилия *"
-        variant="outlined" 
-        placeholder="Введите вашу Фамилию"
-        aria-invalid={errors.patronymic ? 'true' : 'false'}
-        />
-        {errors.patronymic && (
-        <span role='alert' className='error'>
-          {errors.patronymic?.message}
-        </span>
-        )}
-        <TextField
-        {...register('email')}
-        disabled={isSubmitting}
-        id='email'
-        fullWidth margin='normal'
-        label="Email *" 
-        variant="outlined" 
-        placeholder="Введите ваш email" 
-        aria-invalid={errors.email ? 'true' : 'false'}
-        />
-        {errors.email && (
-        <span role='alert' className='error'>
-          {errors.email?.message}
-        </span>
-        )}
-        <TextField 
-        {...register('password')}
-        disabled={isSubmitting}
-        id='password'
-        type="password" 
-        fullWidth margin='normal' 
-        label="Пароль *" 
-        variant="outlined"
-        placeholder="Введите ваш пароль" 
-        aria-invalid={errors.password ? 'true' : 'false'}
-        />
-        {errors.password && (
-        <span role='alert' className='error'>
-          {errors.password?.message}
-        </span>
-        )}
-        <TextField 
-        {...register('confirmPassword')}
-        disabled={isSubmitting}
-        id='confirmPassword'
-        type="password" 
-        fullWidth margin='normal' 
-        label="Повторите пароль *" 
-        variant="outlined" 
-        placeholder="Повторите ваш пароль" 
-        aria-invalid={errors.confirmPassword ? 'true' : 'false'}
-        />
-          {errors.confirmPassword && (
-        <span role='alert' className='error'>
-          {errors.confirmPassword?.message}
-        </span>
-        )}
-        <Button
-        type="submit"
-        className="register-button"
-        sx={{fontFamily:'Popins',marginTop: 2, marginBottom: 2, width: '60%'}}
-        variant="contained"
-        disabled={!isDirty || isSubmitting}
-        onClick={() => console.log("Регистрация")}
-        >
+    <div className="root">
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          margin: 'auto',
+          width: '30%',
+          padding: 20,
+          borderRadius: 10,
+          boxShadow: '5px 5px 10px #ccc',
+        }}
+      >
+        <Typography variant="h2" fontFamily="Popins" textAlign="center">
           Регистрация
-        </Button>
-        <Typography variant="body1" sx={{fontFamily:'Popins'}}>У вас есть аккаунт?<span className="incitingText">Авторизация</span></Typography>
+        </Typography>
+        <Typography
+          variant="body1"
+          marginBottom={3}
+          fontFamily="Popins"
+          textAlign="center"
+        >
+          Введите данные для регистрации
+        </Typography>
+        <form className="form" onSubmit={handleSubmit(onSubmit)}>
+          <div
+            style={{
+              display: 'flex',
+              flex: '1 1 auto',
+              flexDirection: 'column',
+              gap: '10px',
+            }}
+          >
+            <TextField
+              /* eslint-disable react/jsx-props-no-spreading */
+              {...register('username')}
+              disabled={isSubmitting}
+              id="username"
+              label="Имя*"
+              variant="outlined"
+              placeholder="Введите ваше Имя"
+              aria-invalid={errors.username ? 'true' : 'false'}
+              error={!!errors.username}
+              onBlur={() => trigger('username')}
+            />
+            {errors.username && (
+              <span role='alert' className='error'>
+              {errors.username?.message}
+              </span>
+            )}
+            <TextField
+              {...register('surname')}
+              disabled={isSubmitting}
+              id="surname"
+              label="Отчество*"
+              variant="outlined"
+              placeholder="Введите ваше Отчество"
+              aria-invalid={errors.surname ? 'true' : 'false'}
+              error={!!errors.surname}
+              onBlur={() => trigger('surname')}
+            />
+            {errors.surname && (
+              <span role='alert' className='error'>
+              {errors.surname?.message}
+              </span>
+            )}
+            <TextField
+              {...register('patronymic')}
+              disabled={isSubmitting}
+              id="patronymic"
+              label="Фамилия*"
+              variant="outlined"
+              placeholder="Введите вашу Фамилию"
+              aria-invalid={errors.patronymic ? 'true' : 'false'}
+              error={!!errors.patronymic}
+              onBlur={() => trigger('patronymic')}
+            />
+            {errors.patronymic && (
+              <span role='alert' className='error'>
+              {errors.patronymic?.message}
+              </span>
+            )}
+            <TextField
+              {...register('email')}
+              disabled={isSubmitting}
+              id="email"
+              label="Email*"
+              variant="outlined"
+              placeholder="Введите ваш email"
+              aria-invalid={errors.email ? 'true' : 'false'}
+              error={!!errors.email}
+              onBlur={() => trigger('email')}
+            />
+            {errors.email && (
+              <span role='alert' className='error'>
+              {errors.email?.message}
+              </span>
+            )}
+            <TextField
+              {...register('password')}
+              disabled={isSubmitting}
+              id="password"
+              type="password"
+              label="Пароль*"
+              variant="outlined"
+              placeholder="Введите ваш пароль"
+              aria-invalid={errors.password ? 'true' : 'false'}
+              error={!!errors.password}
+              onBlur={() => trigger('password')}
+            />
+            {errors.password && (
+              <span role='alert' className='error'>
+              {errors.password?.message}
+              </span>
+            )}
+            <TextField
+              {...register('confirmPassword')}
+              disabled={isSubmitting}
+              id="confirmPassword"
+              type="password"
+              label="Повторите пароль*"
+              variant="outlined"
+              placeholder="Повторите ваш пароль"
+              aria-invalid={errors.confirmPassword ? 'true' : 'false'}
+              error={!!errors.confirmPassword}
+              onBlur={() => trigger('confirmPassword')}
+            />
+            {errors.confirmPassword && (
+              <span role='alert' className='error'>
+              {errors.confirmPassword?.message}
+              </span>
+            )}
+            <Button
+              type="submit"
+              className="register-button"
+              sx={{
+                fontFamily: 'Popins',
+                marginTop: 2,
+                marginBottom: 2,
+                width: '80%',
+                margin: '0 auto',
+              }}
+              variant="contained"
+              disabled={!isDirty || isSubmitting}
+              onSubmit={() => console.log('Регистрация')}
+            >
+              Регистрация
+            </Button>
+            <Typography
+              variant="body1"
+              textAlign="center"
+              sx={{ fontFamily: 'Popins' }}
+            >
+              У вас есть аккаунт?
+              <span className="incitingText">Авторизация</span>
+            </Typography>
+          </div>
         </form>
-      </Box>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Register;
+export type ShemaType = z.infer<typeof formSchema>;
